c0e1a432d2ecc7f65bfa9f7126567f127dd0c546
2V_Rufin M. <rufintovi11@gmail.com>
2026-01-14 16:31:43 +0800

    remove

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -2,16 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <table style={{ width: '50%', margin: 'auto', textAlign: 'center' , borderCollapse: 'collapse' }} border="1">
-      <tr>
-        <td><h1>Hello</h1></td>
-        <td><h1>World</h1></td>
-      </tr>
-      <tr>
-        <td><p>This is a sample React application.</p></td>
-        <td><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></td>
-      </tr>
-    </table>
+    <h1>test</h1>
   )
 }
 
